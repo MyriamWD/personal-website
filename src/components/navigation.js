9fc4c35f93dba3d4767ac0be3869796b4a65fb6b
Myriam Walden-Duarte <myriam.waldend@gmail.com>
2019-10-01 23:55:04 -0700

    navigation

---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function navigation () {
+    return (
+        <nav>
+            <ul>
+                <li>About me</li>
+                <li>Contact me</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default navigation;
