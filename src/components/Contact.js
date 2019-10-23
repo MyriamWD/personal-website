be5d24575aceabd935d643d58a6a1e1f81d7bbe8
Myriam Walden-Duarte <myriam.waldend@gmail.com>
2019-10-23 00:11:05 -0700

    linkedin and github links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { ReactComponent as GithubLogo } from '../images/github-logo.svg';
+import { ReactComponent as LinkedinLogo } from '../images/linkedin-logo.svg';
 import './Contact.css'
 
 
@@ -10,6 +12,7 @@ export default class Contact extends Component {
         super(props)
         this.state = {
             emailCopied: false,
+            hint: 'Copy email to clipboard',
         }
     }
     
@@ -17,24 +20,44 @@ export default class Contact extends Component {
         navigator.clipboard.writeText("myriam.waldend@gmail.com")
         this.setState({
             emailCopied: true,
+            hint: 'Email copied, yay!'
         })
-      }
 
-      outFunc() {
-        var tooltip = document.getElementById("myTooltip");
-        tooltip.innerHTML = "Copy to clipboard";
-      }
+        setTimeout(() => {
+            this.setState({
+                emailCopied: false,
+                hint: 'Copy email to clipboard',
+            });
+        }, 2000);
+    }
     
     render () {
         return (
             <section className='contact-section' id='contact'>
                 <section className="contact-info">
+                
                     <p className="contact-title">Let's connect!</p>
-                    <p className="contact-description">New projects are always exciting, especially if they involve full-stack development.</p>
-                        <button className={this.state.emailCopied? "contact-copied" : "contact-button"} onClick={this.copyCodeToClipboard} >
-                            <span className="copy-hint">Copy email to clipboard</span>
-                            Say Hello
-                        </button>
+                    <p className="contact-description">
+                    New projects are always exciting, especially if they involve full-stack development.
+                    </p>
+                    <p className="contact-description">
+                    New projects are always exciting, especially if they involve full-stack development.
+                    </p>
+                    <p className="contact-description">
+                        especially if they involve full-stack development.
+                    </p>
+                    <div>
+                        <a href="https://github.com/MyriamWD" target="_blank" rel="noopener noreferrer">
+                            <GithubLogo className="contact-logo"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/myriam-walden-duarte/" target="_blank" rel="noopener noreferrer">
+                            <LinkedinLogo className="contact-logo"/>
+                        </a>
+                    </div>
+                    <button className={this.state.emailCopied? "contact-copied" : "contact-button"} onClick={this.copyCodeToClipboard} >
+                        <span className="copy-hint">{this.state.hint}</span>
+                         Say Hello
+                    </button>
                 </section>
             </section>
         ) 
